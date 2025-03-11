78466681c42fa78f91b50c743c0680b0c2adf069
tesa15012004 <tesadahat@gmail.com>
2025-03-11 21:57:07 +0800

    Pertemuan 2

---
+++ Pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,25 @@
+
+package pertemuan2;
+
+/**
+ *
+ * @author Dahat
+ * tgl : 11 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Alfani Thesa"; 
+        String nim = "2301010473";
+        
+        System.out.println("Pertemuan 2 Java");
+        System.out.println("***************");
+        System.out.println("Nama :"+nama);
+        System.out.println("NIM  :"+nim);
+        System.out.printf("Nama Saya %S dengan NIM %s, Saat ini sedang belajar pemrograman bahasa java", nama,nim);
+    }
+    
+}
